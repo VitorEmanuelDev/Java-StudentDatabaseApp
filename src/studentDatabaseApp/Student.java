322661396a87e65eb805bb09ac08df585364c05b
vitoremanueldev <veslima3@gmail.com>
2021-07-12 03:03:07 -0300

    Setting up main and Student class

---
+++ src/studentDatabaseApp/Student.java
@@ -0,0 +1,5 @@
+package studentDatabaseApp;
+
+public class Student {
+
+}
---
+++ src/studentDatabaseApp/StudentDatabaseApp.java
@@ -0,0 +1,10 @@
+package studentDatabaseApp;
+
+public class StudentDatabaseApp {
+
+	public static void main(String[] args) {
+	
+		
+	}
+	
+}
