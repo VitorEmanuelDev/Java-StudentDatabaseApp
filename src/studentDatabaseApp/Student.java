5a39da4fc9890cd23771b160fd6961a31d38647c
vitoremanueldev <veslima3@gmail.com>
2021-07-14 00:49:23 -0300

    Setting up input

--- src/studentDatabaseApp/Student.java
+++ src/studentDatabaseApp/Student.java
@@ -1,5 +1,41 @@
 package studentDatabaseApp;
 
+import java.util.Scanner;
+
 public class Student {
 
+	private String firstName;
+	private String lastName;
+	private String gradeYear;
+	private int studentID;
+	private String tuitonBalance;
+	private int costOfCourse = 1000;
+	
+	//Constructor: prompt user to enter student's name and year
+		public Student() {
+			
+			Scanner scanner = new Scanner(System.in);
+			System.out.println("Enter student first name: ");
+			this.firstName = scanner.nextLine();
+			
+			System.out.println("Enter student last name: ");
+			this.lastName = scanner.nextLine();
+			
+			System.out.println("1. First year\n2. Second year\n3. Third year\n4. Fourth year\nEnter student level: ");
+			this.gradeYear = scanner.nextLine();
+			
+			System.out.println(firstName + " " + lastName + " " + gradeYear);
+			
+			
+		}
+	//Generate an ID
+		
+	//Enroll in courses
+	
+	//View balance
+	
+	//Pay tuition
+	
+	//Show status
+	
 }
--- src/studentDatabaseApp/StudentDatabaseApp.java
+++ src/studentDatabaseApp/StudentDatabaseApp.java
@@ -4,6 +4,11 @@ public class StudentDatabaseApp {
 
 	public static void main(String[] args) {
 	
+		Student studentOne = new Student();
+		//Ask how many users are going to be added
+		
+		
+		//Create n number of students
 		
 	}
 	
