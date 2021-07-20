fe106d9ee7441066d9eb2b20591fd68aceef7c12
vitoremanueldev <veslima3@gmail.com>
2021-07-20 00:02:33 -0300

    Implementing enrollment and studenId

--- src/studentDatabaseApp/Student.java
+++ src/studentDatabaseApp/Student.java
@@ -6,10 +6,12 @@ public class Student {
 
 	private String firstName;
 	private String lastName;
-	private String gradeYear;
-	private int studentID;
-	private String tuitonBalance;
-	private int costOfCourse = 1000;
+	private int gradeYear;
+	private String studentID;
+	private String courses;
+	private int tuitionBalance = 0;
+	private static int costOfCourse = 700;
+	private static int id = 1001;
 	
 	//Constructor: prompt user to enter student's name and year
 		public Student() {
@@ -22,15 +24,43 @@ public class Student {
 			this.lastName = scanner.nextLine();
 			
 			System.out.println("1. First year\n2. Second year\n3. Third year\n4. Fourth year\nEnter student level: ");
-			this.gradeYear = scanner.nextLine();
+			this.gradeYear = scanner.nextInt();
 			
-			System.out.println(firstName + " " + lastName + " " + gradeYear);
+			setStudentID();
+					
+			System.out.println(firstName + " " + lastName + " " + gradeYear + " " + studentID);
+			scanner.close();
 			
-			
+						
 		}
 	//Generate an ID
+		private void setStudentID() {
+			
+			id++;
+			//Grade level + String + ID. it will become a string
+			this.studentID = gradeYear + "" + id;
+			
+		}
 		
 	//Enroll in courses
+		public void Enroll() {
+			
+			System.out.println("Enter course to enroll (0 to quit): ");
+			Scanner scanner = new Scanner(System.in);
+			String course = scanner.nextLine();
+			scanner.close();
+			
+			if(course != "Q") {
+				
+				courses = courses + "\n" + course;
+				tuitionBalance = tuitionBalance + costOfCourse;
+				
+			}
+			
+			System.out.println("ENROLLED IN: " + courses);
+			System.out.println("TUITION BALANCE: " + tuitionBalance);
+			
+		}
 	
 	//View balance
 	
